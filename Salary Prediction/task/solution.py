6601236a95f08dbdc65e5e5dd4fc9a29817fb1fa
radosan <radoslaw.sanetra@gmail.com>
2024-01-06 19:09:52 +0100

    included multiple predictors

--- Salary Prediction/task/solution.py
+++ Salary Prediction/task/solution.py
@@ -3,8 +3,7 @@ import requests
 import pandas as pd
 from sklearn.linear_model import LinearRegression
 from sklearn.model_selection import train_test_split
-from sklearn.metrics import mean_absolute_percentage_error as mape
-import matplotlib.pyplot as plt
+# from sklearn.metrics import mean_absolute_percentage_error as mape
 
 
 def check_data():
@@ -16,25 +15,14 @@ def check_data():
         open('../Data/data.csv', 'wb').write(r.content)
 
 
-def visualize(x, y, pred):
-        fig, ax = plt.subplots()
-        ax.scatter(x, y)
-        ax.plot(x, pred)
-        plt.show()
-
-
 def main():
     check_data()
     df = pd.read_csv('../Data/data.csv')
-    scores = {}
-    for power in range(1, 5):
-        X, y = df[['rating']] ** power, df['salary']
-        X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=100)
-        model = LinearRegression()
-        model.fit(X_train, y_train)
-        prediction = model.predict(X_test)
-        scores[power] = round(mape(y_test, prediction), 5)
-    print(min(scores.values()))
+    X, y = df.drop('salary', axis=1), df['salary']
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=100)
+    model = LinearRegression()
+    model.fit(X_train, y_train)
+    print(*model.coef_, sep=', ')
 
 
 if __name__ == '__main__':
